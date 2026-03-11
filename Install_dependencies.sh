d1b8ccabc64414b05d8c3dba5cadee8b0bc376d3
Pvcooo <pacomarortiz@gmail.com>
2026-03-11 13:58:25 +0100

    Add files via upload

---
+++ Install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y build-essential g++ make cmake git pkg-config libbz2-dev zlib1g-dev libpng-dev libgdal-dev gdal-bin imagemagick libspdlog-dev python3 python3-venv python3-pip
